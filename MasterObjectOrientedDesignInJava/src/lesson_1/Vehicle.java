7da55e1467ea760dafb76c2fa2e2dd853a3678bf
Martin <1135485226@qq.com>
2019-07-16 18:35:30 +0800

    created a vehicle class

---
+++ MasterObjectOrientedDesignInJava/src/lesson_1/Vehicle.java
@@ -0,0 +1,15 @@
+package lesson_1;
+
+public class Vehicle {
+    public void startEngine(){
+        System.out.println("Starting the engine");
+    }
+
+    public void stopEngine(){
+        System.out.println("Stopping the engine");
+    }
+
+    public void accelerate(){
+        System.out.println("The car is accelerating");
+    }
+}
